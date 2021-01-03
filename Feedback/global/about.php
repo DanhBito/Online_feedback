aba70dee743ed136185f546dd453fa064e9aa9a6
Dương Duy Danh <42829697+DanhBito@users.noreply.github.com>
2021-01-03 19:29:41 +0700

    Add files via upload
    add forder global

---
+++ Feedback/global/about.php
@@ -0,0 +1,12 @@
+<div class="about ">
+            <div class="title">
+                <h2>Tổng Quan</h2>
+                <h3>Hệ Thống Phản Hồi cho sinh viên Trường ĐH Thông Tin Liên Lạc bằng PHP</h3>
+            </div>
+            <div class="content">
+                <p>Ở đây chúng tôi phát triển hệ thống phản hồi của giảng viên, thường được sử dụng để đánh giá giảng viên dựa trên hiệu suất....Ở đây chúng tôi có 2 mô-đun là <b>quản trị viên</b> và <b>sinh viên</b>.</p>
+                <p><b style="color: #ff0000dc;">Quản trị viên </b>là người tạo tài khoản sinh viên bằng cách thêm tất cả thông tin sinh viên và chỉ định tên người dùng và mật khẩu. Quản trị viên sẽ kiểm tra kết quả sau khi tất cả sinh viên đã nhập phản hồi</p>
+                <p><b style="color: #ff0000dc;">Sinh viên </b>là người đưa ra đánh giá về các giảng viên trên tinh thần khách quan và góp ý. Sinh viên sẽ được người quản trị viên cấp phát tài khoản và mật khẩu để đăng nhập.</p>
+            </div>
+        </div>
+    </div>
---
+++ Feedback/global/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+        <div class="fixed-bottom">
+            <p>2020 - <strong>Telecommunications University</strong> - FeedBack with PHP</p>
+        </div>
+    </div>
