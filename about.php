ce35ea448e8e2ed9fa2073bf3be839f78d2c0b0d
Dương Duy Danh <42829697+DanhBito@users.noreply.github.com>
2021-01-14 21:49:49 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,27 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">HỆ THỐNG PHẢN HỒI CHO SINH VIÊN</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+            <P>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente eaque quos qui aliquid perferendis natus saepe vero quae necessitatibus harum commodi quas veritatis a quo, tenetur distinctio rem impedit?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente eaque quos qui aliquid perferendis natus saepe vero quae necessitatibus harum commodi quas veritatis a quo, tenetur distinctio rem impedit?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente eaque quos qui aliquid perferendis natus saepe vero quae necessitatibus harum commodi quas veritatis a quo, tenetur distinctio rem impedit?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente eaque quos qui aliquid perferendis natus saepe vero quae necessitatibus harum commodi quas veritatis a quo, tenetur distinctio rem impedit?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sapiente eaque quos qui aliquid perferendis natus saepe vero quae necessitatibus harum commodi quas veritatis a quo, tenetur distinctio rem impedit?
+            </P> 
+          
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
