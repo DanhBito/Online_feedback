34aeff7effbcd1f081baea28d1c02f9bb59245d0
Dương Duy Danh <42829697+DanhBito@users.noreply.github.com>
2021-01-25 20:03:08 +0700

    Add files via upload

--- database/feedback_system.sql
+++ database/feedback_system.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2018 at 10:31 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Jan 25, 2021 at 02:02 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `feedback_system`
@@ -26,12 +28,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
   `user` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -46,23 +47,22 @@ INSERT INTO `admin` (`admin_id`, `user`, `pass`) VALUES
 -- Table structure for table `contact`
 --
 
-CREATE TABLE IF NOT EXISTS `contact` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mobile` bigint(20) NOT NULL,
   `message` text NOT NULL,
-  `Date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `Date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contact`
 --
 
 INSERT INTO `contact` (`id`, `name`, `email`, `mobile`, `message`, `Date`) VALUES
-(5, 'dfd', 'sanjeevtech2@gmail.com', 9015501897, 'ddd', '2016-06-29 17:53:28'),
-(6, 'dfd', 'sanjeevtech2@gmail.com', 9015501897, 'ddd', '2016-06-29 17:53:43');
+(5, 'DuyDanh', 'duydanh@gmail.com', 337136172, 'My name\'s Duy Danh', '2020-11-29 17:53:28'),
+(6, 'Minh Khoi', 'minhkhoi@gmail.com', 123456789, 'My name\'s Khoi', '2020-11-29 17:53:43');
 
 -- --------------------------------------------------------
 
@@ -70,8 +70,8 @@ INSERT INTO `contact` (`id`, `name`, `email`, `mobile`, `message`, `Date`) VALUE
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE IF NOT EXISTS `faculty` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
   `user_alias` varchar(30) NOT NULL,
   `Name` varchar(30) NOT NULL,
   `designation` varchar(100) NOT NULL,
@@ -81,19 +81,18 @@ CREATE TABLE IF NOT EXISTS `faculty` (
   `password` varchar(75) NOT NULL,
   `mobile` bigint(20) NOT NULL,
   `date` datetime NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `faculty`
 --
 
 INSERT INTO `faculty` (`id`, `user_alias`, `Name`, `designation`, `programme`, `semester`, `email`, `password`, `mobile`, `date`, `status`) VALUES
-(7, 'ravm5454', 'ravi', 'Associate Professior', 'B.Tech', 'ii', 'rav@gmail.com', 'ravi', 9015501897, '2016-07-13 14:30:53', 0),
-(8, 'sanj9015', 'sanjeev kumar', 'Developer', 'B.tech', 'ii', 'sanjeevtech2@gmail.com', '2ddea1', 9015501897, '2016-07-13 14:37:35', 0),
-(11, 'sanj9015', 'sanjeev kuma', 'aaaa', 'B.tec', 'i', 'sanjeevtechh2@gmail.com', 'dfdfd', 901550189, '2016-07-13 14:40:35', 1);
+(7, 'duydanh', 'Duy Danh', 'Chu nhiem khoa', 'Lap trinh web', 'ii', 'duydanh@gmail.com', 'duydanh', 337136172, '2020-11-21 14:30:53', 0),
+(8, 'minhkhoi', 'Chau Hoang Minh Khoi', 'Pho Chu Nhiem Khoa', 'Lap trinh web', 'ii', 'minhkhoi@gmail.com', 'minhkhoi', 345667891, '2020-01-21 14:37:35', 0),
+(11, 'quockhang', 'Nguyen The Quoc Khang', 'Giang Vien', 'Lap trinh web', 'i', 'quockhang@gmail.com', 'quockhang', 123456789, '2020-11-21 14:40:35', 1),
+(12, 'test4337', 'test', 'test', 'ABC', 'i', 'test@gmail.com', '123', 123456789, '2020-11-26 08:01:03', 0);
 
 -- --------------------------------------------------------
 
@@ -101,36 +100,34 @@ INSERT INTO `faculty` (`id`, `user_alias`, `Name`, `designation`, `programme`, `
 -- Table structure for table `feedback`
 --
 
-CREATE TABLE IF NOT EXISTS `feedback` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
   `student_id` varchar(50) NOT NULL,
   `faculty_id` varchar(50) NOT NULL,
-  `Teacher provided the course outline having weekly content plan w` enum('5','4','3','2','1') NOT NULL,
-  `Course objectives,learning outcomes and grading criteria are cle` enum('5','4','3','2','1') NOT NULL,
-  `Course integrates throretical course concepts with the real worl` enum('5','4','3','2','1') NOT NULL,
-  `Teacher is punctual,arrives on time and leaves on time` enum('5','4','3','2','1') NOT NULL,
-  `Teacher is good at stimulating the interest in the course conten` enum('5','4','3','2','1') NOT NULL,
-  `Teacher is good at explaining the subject matter` enum('5','4','3','2','1') NOT NULL,
-  `Teacher's presentation was clear,loud ad easy to understand` enum('5','4','3','2','1') NOT NULL,
-  `Teacher is good at using innovative teaching methods/ways` enum('5','4','3','2','1') NOT NULL,
-  `Teacher is available and helpful during counseling hours` enum('5','4','3','2','1') NOT NULL,
-  `Teacher has competed the whole course as per course outline` enum('5','4','3','2','1') NOT NULL,
-  `Teacher was always fair and impartial:` enum('5','4','3','2','1') NOT NULL,
-  `Assessments conducted are clearly connected to maximize learinin` enum('5','4','3','2','1') NOT NULL,
-  `What I liked about the course` text NOT NULL,
-  `Why I disliked about the course` text NOT NULL,
-  `date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+  `Question1` enum('5','4','3','2','1') NOT NULL,
+  `Question2` enum('5','4','3','2','1') NOT NULL,
+  `Question3` enum('5','4','3','2','1') NOT NULL,
+  `Question4` enum('5','4','3','2','1') NOT NULL,
+  `Question5` enum('5','4','3','2','1') NOT NULL,
+  `Question6` enum('5','4','3','2','1') NOT NULL,
+  `Question7` enum('5','4','3','2','1') NOT NULL,
+  `Question8` enum('5','4','3','2','1') NOT NULL,
+  `Question9` enum('5','4','3','2','1') NOT NULL,
+  `Question10` enum('5','4','3','2','1') NOT NULL,
+  `Question11` enum('5','4','3','2','1') NOT NULL,
+  `Question12` enum('5','4','3','2','1') NOT NULL,
+  `Question13` text NOT NULL,
+  `Question14` text NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `feedback`
 --
 
-INSERT INTO `feedback` (`id`, `student_id`, `faculty_id`, `Teacher provided the course outline having weekly content plan w`, `Course objectives,learning outcomes and grading criteria are cle`, `Course integrates throretical course concepts with the real worl`, `Teacher is punctual,arrives on time and leaves on time`, `Teacher is good at stimulating the interest in the course conten`, `Teacher is good at explaining the subject matter`, `Teacher's presentation was clear,loud ad easy to understand`, `Teacher is good at using innovative teaching methods/ways`, `Teacher is available and helpful during counseling hours`, `Teacher has competed the whole course as per course outline`, `Teacher was always fair and impartial:`, `Assessments conducted are clearly connected to maximize learinin`, `What I liked about the course`, `Why I disliked about the course`, `date`) VALUES
-(16, 'ravi@gmail.com', 'rav@gmail.com', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '\r\nddddddddddddd', 'aa', '2016-07-15'),
-(17, 'sanjeevtech2@gmail.com', 'rav@gmail.com', '5', '3', '1', '5', '5', '3', '3', '3', '3', '5', '5', '5', '\r\n', '\r\n', '2016-07-15'),
-(18, 'warda@yahoo.com', 'rav@gmail.com', '5', '5', '5', '2', '1', '5', '5', '4', '5', '5', '5', '5', '\r\ndfdfdfdfdfd', '\r\n', '2016-07-17');
+INSERT INTO `feedback` (`id`, `student_id`, `faculty_id`, `Question1`, `Question2`, `Question3`, `Question4`, `Question5`, `Question6`, `Question7`, `Question8`, `Question9`, `Question10`, `Question11`, `Question12`, `Question13`, `Question14`, `date`) VALUES
+(19, 'student1@gmail.com', 'duydanh@gmail.com', '5', '5', '4', '5', '5', '5', '4', '3', '3', '2', '5', '5', '\r\nabc', '\r\nabc', '2021-01-14'),
+(20, 'student1@gmail.com', 'minhkhoi@gmail.com', '4', '4', '4', '5', '5', '3', '3', '2', '2', '5', '4', '5', '\r\n', '\r\n', '2021-01-15');
 
 -- --------------------------------------------------------
 
@@ -138,21 +135,13 @@ INSERT INTO `feedback` (`id`, `student_id`, `faculty_id`, `Teacher provided the 
 -- Table structure for table `files`
 --
 
-CREATE TABLE IF NOT EXISTS `files` (
-  `notice_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `files` (
+  `notice_id` int(11) NOT NULL,
   `attachment` varchar(255) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `Description` text NOT NULL,
-  `Date` datetime NOT NULL,
-  PRIMARY KEY (`notice_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`notice_id`, `attachment`, `subject`, `Description`, `Date`) VALUES
-(8, 'AteekCV_java (1).docx', 'aaaaa', 'dfdfdfd', '2016-07-03 12:39:35');
+  `Date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -160,8 +149,8 @@ INSERT INTO `files` (`notice_id`, `attachment`, `subject`, `Description`, `Date`
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `name` char(40) NOT NULL,
   `email` varchar(40) NOT NULL,
   `pass` varchar(40) NOT NULL,
@@ -172,21 +161,99 @@ CREATE TABLE IF NOT EXISTS `user` (
   `hobbies` varchar(40) NOT NULL,
   `image` varchar(50) NOT NULL,
   `dob` date NOT NULL,
-  `regid` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  FULLTEXT KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+  `regid` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `pass`, `mobile`, `programme`, `semester`, `gender`, `hobbies`, `image`, `dob`, `regid`) VALUES
-(10, 'sanjeev kumar', 'sanjeevtech2@gmail.com', '98d34c1758b15b5a359b69c2b08c5767', 9015501897, 'B.tech', '3rd', 'm', 'reading,playing', 'Jellyfish.jpg', '1961-09-15', '2147483647'),
-(12, 'ravi', 'ravi@gmail.com', '63dd3e154ca6d948fc380fa576343ba6', 9015501897, 'M.Tech', 'ii', 'm', 'reading', 'Desert.jpg', '1965-10-15', '2016-07-13 15:52:01'),
-(13, 'warda', 'warda@yahoo.com', '827ccb0eea8a706c4c34a16891f84e7b', 32457895212, 'BCA', 'ii', 'f', 'reading', 'Koala - Copy.jpg', '1965-10-06', '2016-07-17 15:39:08'),
-(14, 'test', 'test@gmail.com', '098f6bcd4621d373cade4e832627b4f6', 989898989, 'MCA', 'i', 'm', 'reading,singin', 'Chrysanthemum.jpg', '1963-08-12', '2017-02-10 16:04:10');
+(15, 'Duy Danh', 'student1@gmail.com', '5e5545d38a68148a2d5bd5ec9a89e327', 337136172, 'BCA', 'ii', 'm', 'playing', '1.jpg', '1999-12-20', '2021-01-14 21:07:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`notice_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `user` ADD FULLTEXT KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `notice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
